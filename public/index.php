676efe426cbbf5484fb22ef7c2bb8c83298cf406
Slavomir Sidor <slavomir.sidor@gmail.com>
2019-12-13 20:07:15 +0100

    de

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use SS\App\Kernel;
+use SS\Application\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
--- src/SS/Application/Kernel.php
+++ src/SS/Application/Kernel.php
@@ -1,5 +1,12 @@
 <?php
-namespace SS\App;
+/**
+ * SS Application Kernel
+ *
+ * @licence GNU GPL
+ *
+ * @author Slavomir <slavomir.sidor@gmail.com>
+ */
+namespace SS\Application;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
@@ -12,11 +19,21 @@ class Kernel extends BaseKernel
 {
 	use MicroKernelTrait;
 
-	private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
+	/**
+	 *
+	 * @var string
+	 */
+	private const CONFIG_EXTS = '.{php,xml}';
 
+	/**
+	 *
+	 * {@inheritdoc}
+	 * @see \Symfony\Component\HttpKernel\KernelInterface::registerBundles()
+	 */
 	public function registerBundles(): iterable
 	{
 		$contents = require $this->getProjectDir() . '/config/bundles.php';
+
 		foreach( $contents as $class => $envs )
 		{
 			if( $envs[$this->environment] ?? $envs['all'] ?? false)
@@ -26,39 +43,45 @@ class Kernel extends BaseKernel
 		}
 	}
 
+	/**
+	 *
+	 * {@inheritdoc}
+	 * @see \Symfony\Component\HttpKernel\Kernel::getProjectDir()
+	 */
 	public function getProjectDir(): string
 	{
 		$projectDir = \dirname( __DIR__, 3 );
 		return $projectDir;
 	}
 
-	protected function configureContainer( ContainerBuilder $container,
-		LoaderInterface $loader ): void
+	/**
+	 *
+	 * @param ContainerBuilder $container
+	 * @param LoaderInterface $loader
+	 */
+	protected function configureContainer( ContainerBuilder $container, LoaderInterface $loader ): void
 	{
-		$container->addResource( 
-			new FileResource( $this->getProjectDir() . '/config/bundles.php' ) );
+		$container->addResource( new FileResource( $this->getProjectDir() . '/config/bundles.php' ) );
+
 		$container->setParameter( 'container.dumper.inline_class_loader',
 			\PHP_VERSION_ID < 70400 || ! ini_get( 'opcache.preload' ) );
+
 		$container->setParameter( 'container.dumper.inline_factories', true );
+
 		$confDir = $this->getProjectDir() . '/config';
 
-		$loader->load( $confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob' );
-		$loader->load( 
-			$confDir . '/{packages}/' . $this->environment . '/*' . self::CONFIG_EXTS,
-			'glob' );
-		$loader->load( $confDir . '/{services}' . self::CONFIG_EXTS, 'glob' );
-		$loader->load( $confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS,
-			'glob' );
+		$loader->load( $confDir . '/{packages}' . self::CONFIG_EXTS, 'glob' );
+		$loader->load( $confDir . '/' . $this->environment . '/{packages}' . self::CONFIG_EXTS, 'glob' );
 	}
 
+	/**
+	 *
+	 * @param RouteCollectionBuilder $routes
+	 */
 	protected function configureRoutes( RouteCollectionBuilder $routes ): void
 	{
 		$confDir = $this->getProjectDir() . '/config';
-
-		$routes->import( 
-			$confDir . '/{routes}/' . $this->environment . '/*' . self::CONFIG_EXTS, '/',
-			'glob' );
-		$routes->import( $confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob' );
-		$routes->import( $confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob' );
+		$routes->import( $confDir . '/routes' . self::CONFIG_EXTS, '/', 'glob' );
+		$routes->import( $confDir . '/' . $this->environment . '/routes' . self::CONFIG_EXTS, '/', 'glob' );
 	}
 }

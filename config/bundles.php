ba2e6e78e144b023b4c63979390ee758c69561e6
Slavomir Sidor <slavomir.sidor@gmail.com>
2019-12-13 04:42:31 +0100

    namespace

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 	SS\Bundle\MAPIBundle\MAPIBundle::class => ['all' => true],
